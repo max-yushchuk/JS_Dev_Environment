49a4179b3d903d738e7c7c26b700c3d8a8b767fb
Maxim Yushchuk <myuschuk@softserveinc.com>
2017-03-28 15:36:50 +0300

    Add delete method

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,9 +7,20 @@ export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 function get(url) {
-  console.log(baseUrl);
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+// Can't call func delete since reserved word
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
